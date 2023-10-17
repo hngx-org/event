08a48993231bf21c1e21fd92755ad387bbbf1dda
sultyl <70979325+sultyl@users.noreply.github.com>
2023-10-17 22:27:29 +0100

    added event details page

--- pages/EventDetails.tsx
+++ pages/EventDetails.tsx
@@ -16,39 +16,41 @@ import Link from "next/link";
 export default function EventDetails () {
     return (
         <>
-            <header className='max-w-7xl mx-auto flex justify-between p-4 xl:px-0'>
-                <div className='flex gap-20 items-center'>
-                    {/* Logo */}
-                    <Link href='/'>
-                    <Image
-                        src={Logo}
-                        alt='logo'
-                        width={133}
-                        height={16}
-                        className='h-auto w-auto'
-                    />
-                    </Link>                    
-                    <form className="w-[380px]">   
-                        <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                        <div className="relative">
-                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                </svg>
+            <header className='fixed top-0 left-0 right-0 bg-white z-10'>
+                <div className="max-w-7xl mx-auto flex justify-between p-4 xl:px-0 ">
+                    <div className='flex gap-20 items-center'>
+                        {/* Logo */}
+                        <Link href='/'>
+                        <Image
+                            src={Logo}
+                            alt='logo'
+                            width={133}
+                            height={16}
+                            className='h-auto w-auto'
+                        />
+                        </Link>                    
+                        <form className="w-[380px]">   
+                            <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                            <div className="relative">
+                                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                    </svg>
+                                </div>
+                                <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg" placeholder="Search for more events" required />
                             </div>
-                            <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg" placeholder="Search for more events" required />
-                        </div>
-                    </form>
+                        </form>
 
-                </div>
-                <div className='flex gap-4 items-center'>
-                    <Image 
-                        src={user}
-                        width={40}
-                        height={40}
-                        alt="user"
-                    />
-                    <h3 className="text-base font-semibold">Username</h3>
+                    </div>
+                    <div className='flex gap-4 items-center'>
+                        <Image 
+                            src={user}
+                            width={40}
+                            height={40}
+                            alt="user"
+                        />
+                        <h3 className="text-base font-semibold">Username</h3>
+                    </div>
                 </div>
 
                 {/* Mobile menu toggle */}
@@ -56,7 +58,7 @@ export default function EventDetails () {
                     <HamburgerIcon />
                 </button>
             </header>
-            <div className="max-w-7xl mx-auto flex justify-between mb-6 mt-8">
+            <div className="max-w-7xl mx-auto flex justify-between mb-6 mt-24">
                 <div className="flex gap-4 items-center">
                     <ArrowBackIcon />
                     <h3 className="text-[#3B3B3B] text-2xl font-bold">Tech Innovators Summit</h3>

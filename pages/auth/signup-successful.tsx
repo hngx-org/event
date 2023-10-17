6dbef4e013a7492113bbbb5948143e310636ad1f
AJAYI Toheeb Opeyemi <toheebopeyemi9@gmail.com>
2023-10-17 10:55:02 +0100

    SignUp - Successful Page Created

---
+++ pages/auth/signup-successful.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const signupSuccessful = () => {
+    return (
+        <div>
+            Hello Sign Up successful page
+        </div>
+    )
+}
+
+export default signupSuccessful

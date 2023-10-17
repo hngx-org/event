b6fcc47bb4c80171600bf0639beb2a0b3d9413e3
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-10-17 16:41:51 +0100

    feat: add header to reset password successful page

--- pages/auth/reset-password-successful.tsx
+++ pages/auth/reset-password-successful.tsx
@@ -1,12 +1,18 @@
-import SuccessPage from "@/components/successPage"
+import SuccessPage from "@/components/successPage";
+import Header from "@/components/web/header";
 
 export default function ResetPassWordSuccesful() {
   return (
-    <SuccessPage
-      route="login"
-      title="Reset password successful" 
-      text="Your password has been successfully reset. Click below to log in."
-      button="Back to Login" 
-    />
+    <div>
+      <div className="lg:hidden">
+        <Header />
+      </div>
+      <SuccessPage
+        route="login"
+        title="Reset password successful" 
+        text="Your password has been successfully reset. Click below to log in."
+        button="Back to Login" 
+      />
+    </div>
   )
 }

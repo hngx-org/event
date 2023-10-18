8255f7aaa22d4969ce8092f6897d513957191f36
Judge-Paul <judgepaulogebe3@gmail.com>
2023-10-18 09:24:57 +0100

    feat: create event card

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,6 +1,19 @@
+import { StaticImageData } from "next/image";
+
 export type SuccessProps = {
-  title: string,
-  text: string,
-  route: string,
-  button: string
-}
+  title: string;
+  text: string;
+  route: string;
+  button: string;
+};
+
+export type EventCardProps = {
+  img: StaticImageData | string;
+  title: string;
+  location: string;
+  time: string;
+  month: string;
+  day: string;
+  isLive: boolean;
+  cost: number;
+};

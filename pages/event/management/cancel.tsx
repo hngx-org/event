3d2fdeed3b851d817660dbb54c2291aa4ba28e94
AJAYI Toheeb Opeyemi <toheebopeyemi9@gmail.com>
2023-10-19 13:17:30 +0100

    Event Management Cancel Page Created

---
+++ pages/event/management/cancel.tsx
@@ -0,0 +1,12 @@
+import EventLayout from '@/components/layout/eventLayout'
+import React from 'react'
+
+const Cancel = () => {
+  return (
+    <EventLayout>
+        cancel
+    </EventLayout>
+  )
+}
+
+export default Cancel

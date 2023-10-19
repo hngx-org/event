d436104459e76c04c6c61f9b8f05bfdb9d9bbfa2
Judge-Paul <judgepaulogebe3@gmail.com>
2023-10-19 12:25:33 +0100

    feat: create event loading card

--- components/eventHeader.tsx
+++ components/eventHeader.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import Logo from "@/public/assets/images/logo.png";
-import user from "@/public/assets/images/user.svg";
+import avatar from "@/public/assets/images/user.svg";
 import search from "@/public/images/search.svg";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useSearchParams } from "next/navigation";
+import { useAuth } from "@/hooks/useAuth";
 
 const EventHeader = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
+  const { user } = useAuth();
   const searchQuery = searchParams.get("search") || "";
 
   const [showSearch, setShowSearch] = useState(false);
@@ -47,8 +49,45 @@ const EventHeader = () => {
           >
             <Image src={search} alt="search" className="h-5 w-5 " />
           </button>
-          <Image src={user} alt="user" className="h-[40px] w-[40px]" />
-          <p className="font-semibold truncate">User's Name</p>
+          <Image
+            src={user?.avatar || avatar}
+            alt="avatar"
+            width={40}
+            height={40}
+            className="rounded-full"
+            unoptimized
+          />
+          <p className="font-semibold truncate">
+            {user?.username.split(" ")[0] || "User"}
+          </p>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M11.9966 15.1037L5.47374 8.2704C5.30166 8.09013 5.09906 8.00233 4.86592 8.00698C4.63279 8.01164 4.43019 8.1041 4.25811 8.28437C4.08604 8.46462 4 8.67687 4 8.9211C4 9.16534 4.08604 9.37758 4.25811 9.55783L10.8875 16.5168C11.0441 16.6808 11.2195 16.8023 11.4137 16.8814C11.608 16.9605 11.8023 17 11.9966 17C12.1909 17 12.3851 16.9605 12.5794 16.8814C12.7737 16.8023 12.9491 16.6808 13.1056 16.5168L19.7484 9.55783C19.9204 9.37758 20.0043 9.16301 19.9998 8.91412C19.9954 8.66524 19.9071 8.45067 19.7351 8.2704C19.563 8.09013 19.3604 8 19.1272 8C18.8941 8 18.6915 8.09013 18.5194 8.2704L11.9966 15.1037Z"
+              fill="#3B3B3B"
+            />
+            <mask
+              id="mask0_1768_2306"
+              maskUnits="userSpaceOnUse"
+              x="0"
+              y="0"
+              width="24"
+              height="24"
+            >
+              <rect
+                width="24"
+                height="24"
+                transform="matrix(0 -1 -1 0 24 24)"
+                fill="#FEFEFE"
+              />
+            </mask>
+            <g mask="url(#mask0_1768_2306)"></g>
+          </svg>
         </div>
       </div>
       {showSearch && (
--- pages/timeline/index.tsx
+++ pages/timeline/index.tsx
@@ -1,12 +1,28 @@
 import WebLayout from "@/components/layout/webLayout";
 import BG from "@/public/assets/images/dashboardBg.png";
 import EventCard from "@/components/eventCard";
+import EventCardLoading from "@/components/eventCardLoading";
 import Event from "@/public/images/event-image.png";
 import Event2 from "@/public/images/event-image-2.png";
 import Event3 from "@/public/images/event-image-3.png";
 import EventLayout from "@/components/layout/eventLayout";
+import Link from "next/link";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { EventCardProps } from "@/@types";
 
 export default function Dashboard() {
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    try {
+      axios
+        .get("https://wetindeysup-api.onrender.com/api/events/upcoming?limit=6")
+        .then((res) => setEvents(res.data.data));
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  console.log(events);
   return (
     <>
       <EventLayout>
@@ -21,14 +37,30 @@ export default function Dashboard() {
             <button className="w-full sm:w-max bg-[#800000] text-white hover:bg-[#800000]/50 sm:mr-3 px-6 py-2.5 rounded-md">
               Create An Event
             </button>
-            <button className="w-full sm:w-max border border-[#800000] mt-5 sm:mt-0 sm:ml-3 text-[#800000] hover:bg-[#800000]/25 hover:text-white px-6 py-2.5 rounded-md">
+            <Link
+              href="/events"
+              className="w-full sm:w-max border border-[#800000] mt-5 sm:mt-0 sm:ml-3 text-[#800000] hover:bg-[#800000]/25 hover:text-white px-6 py-2.5 rounded-md"
+            >
               Explore Other Events
-            </button>
+            </Link>
           </div>
         </div>
         <div className="mt-7 px-8 sm:px-12 md:px-16 lg:px-20">
           <h4 className="text-2xl font-bold">Upcoming Events Near You</h4>
           <div className="mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {events.map((event: any) => (
+              <EventCard
+                key={event.id}
+                title={event.name || "Event Name"}
+                location={event.location || "Not Specified"}
+                img={event.image || Event}
+                cost={event.ticketPrice || 0}
+                isLive={true}
+                day="08"
+                month="OCT"
+                time="9:00 AM"
+              />
+            ))}
             <EventCard
               title="Event Name"
               location="Event Location"
@@ -49,46 +81,9 @@ export default function Dashboard() {
               img={Event2}
               isLive={false}
             />
-            <EventCard
-              title="Event Name"
-              location="Event Location"
-              time="Event Time"
-              month="OCT"
-              day="08"
-              cost={0}
-              img={Event3}
-              isLive={true}
-            />
-            <EventCard
-              title="Event Name"
-              location="Event Location"
-              time="Event Time"
-              month="OCT"
-              day="08"
-              cost={0}
-              img={Event}
-              isLive={true}
-            />
-            <EventCard
-              title="Event Name"
-              location="Event Location"
-              time="Event Time"
-              month="OCT"
-              day="08"
-              cost={0}
-              img={Event}
-              isLive={true}
-            />
-            <EventCard
-              title="Event Name"
-              location="Event Location"
-              time="Event Time"
-              month="OCT"
-              day="08"
-              cost={0}
-              img={Event}
-              isLive={true}
-            />
+            <EventCardLoading />
+            <EventCardLoading />
+            <EventCardLoading />
           </div>
         </div>
       </EventLayout>

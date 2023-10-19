0168a077b7aaadf774b7f6fc6b2c142add8555dd
AJAYI Toheeb Opeyemi <toheebopeyemi9@gmail.com>
2023-10-19 22:48:57 +0100

    DOne

--- pages/event/management/delete.tsx
+++ pages/event/management/delete.tsx
@@ -89,7 +89,7 @@ const Delete = () => {
                     alt="banner"
                     className="w-full max-h-[458px] h-full object-cover  mt-11"
                 />
-                <div className="container mx-auto mt-8 px-[20px] md:px-[40px] mb-[100px]">
+                <div className="container mx-auto mt-8 px-[20px] md:px-[40px] mb-4">
                     {/* Event Description and Date */}
                     <div className="flex flex-col lg:flex-row gap-10 justify-between">
                         {/* Event Description */}
@@ -98,6 +98,32 @@ const Delete = () => {
                             <p className="mt-6 text-[#666666] text-lg font-medium text-justify">
                                 Dive into the world of cutting-edge technology at the Tech Innovators Summit. This immersive event is designed for tech enthusiasts, entrepreneurs, and innovators alike. Join us for a day filled with inspiring keynote speakers, hands-on workshops, and networking opportunities with industry leaders. Discover the latest trends in artificial intelligence, blockchain, cybersecurity, and more. Whether you're a tech veteran or just starting your journey, the Tech Innovators Summit is the ultimate destination to explore, learn, and connect in the dynamic tech landscape.
                             </p>
+                            {/*  */}
+                            <div className="max-w-[650px] w-full flex gap-4 justify-between mt-8">
+                                <div>
+                                    <h1 className="font-montserrat font-bold text-2xl text-[#3B3B3B]">Available Tickets</h1>
+                                    <p className="mt-6 text-[#666666] font-medium">20</p>
+                                </div>
+                                <div>
+                                    <h1 className="font-montserrat font-bold text-2xl text-[#3B3B3B]">Registration Closing Date</h1>
+                                    <p className="mt-6 text-[#666666] font-medium">06/11/2023</p>
+                                </div>
+                            </div>
+                            {/* Event Tags */}
+                            <div className="max-w-[653px] mt-8">
+                                <h1 className="font-bold font-montserrat text-[#3B3B3B] text-2xl">Event Tags</h1>
+                                <div className="mt-6 flex flex-wrap gap-y-4 gap-x-8">
+                                    {eventTags?.map((event, index) => (
+                                        <button
+                                            key={index}
+                                            type='button'
+                                            className="py-2 text-center px-3 border rounded-lg text-[#3B3B3B]"
+                                        >
+                                            {event}
+                                        </button>
+                                    ))}
+                                </div>
+                            </div>
                         </div>
                         {/* Date */}
                         <div className="lg:max-w-[437px] w-full h-max p-6 rounded-2xl bg-[#FAFAFA]">
@@ -121,67 +147,6 @@ const Delete = () => {
                                     <p>In 2 weeks</p>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    {/* Event Tags */}
-                    <div className="max-w-[653px] mt-8">
-                        <h1 className="font-bold font-montserrat text-[#3B3B3B] text-2xl">Event Tags</h1>
-                        <div className="mt-6 flex flex-wrap gap-y-4 gap-x-8">
-                            {eventTags?.map((event, index) => (
-                                <button
-                                    key={index}
-                                    type='button'
-                                    className="py-2 text-center px-3 border rounded-lg text-[#3B3B3B]"
-                                >
-                                    {event}
-                                </button>
-                            ))}
-                        </div>
-                    </div>
-                    {/* Similar Events */}
-                    <div className="mt-[72px]">
-                        <h1 className="text-[#3B3B3B] font-montserrat font-bold text-2xl">Similar Events</h1>
-                        <div className="flex flex-wrap gap-x-5 gap-y-3">
-                            {similarEvents?.map(({ imgSrc, name, location, time, live, amount, date }, index) => (
-                                <div
-                                    key={index}
-                                    className="max-w-[405px] w-full border bg-[#FAFAFA] rounded-2xl"
-                                >
-                                    <Image alt={name} src={imgSrc} />
-                                    <div className="p-6 flex justify-between gap-4">
-                                        <div>
-                                            <h1 className="text-[#0D0D0D] font-bold text-xl font-montserrat">
-                                                {name}
-                                            </h1>
-                                            <div className="flex gap-2 mt-4">
-                                                <LocationSVG />
-                                                <p>{location}</p>
-                                            </div>
-                                            <div className="flex gap-2 mt-4">
-                                                <TimerSVG />
-                                                <p className="font-semibold text-[#1C1C1C]">{time}</p>
-                                            </div>
-                                            {live === "Live" ?
-                                                <div className="flex items-center gap-2 mt-4">
-                                                    <span className="w-2 h-2 rounded-full bg-[#30980C] animate-pulse"></span>
-                                                    <p className="font-bold text-[#30980C]">{live}</p>
-                                                </div>
-                                                : <div className="flex gap-2 mt-4">
-                                                    <CalendarSVG />
-                                                    <p className="font-bold text-[#1C1C1C]">{live}</p>
-                                                </div>
-                                            }
-                                        </div>
-                                        <div className="flex flex-col justify-between items-center font-bold">
-                                            <div className="px-4 py-1 text-center bg-[#F2E6E6] rounded">
-                                                <p className="text-[#962B2B]">{date.split(" ")[0]}</p>
-                                                <p className="text-[#800000] text-2xl font-montserrat font-semibold">{date.split(" ")[1]}</p>
-                                            </div>
-                                            <p className="text-[#1C1C1C]">{amount}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
                         </div>
                     </div>
                 </div>

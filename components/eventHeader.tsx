af9c98ee635a21e5b2bf0ed7713af321cc8f2387
ebukathedev <ebuskelele@gmail.com>
2023-10-19 08:05:16 +0100

    complete search filter modal UI

--- components/eventHeader.tsx
+++ components/eventHeader.tsx
@@ -7,63 +7,70 @@ import { useRouter } from "next/router";
 import { useSearchParams } from "next/navigation";
 
 const EventHeader = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
+	const router = useRouter();
+	const searchParams = useSearchParams();
 
-  const searchQuery = searchParams.get("search") || "";
+	const searchQuery = searchParams.get("search") || "";
 
-  const [showSearch, setShowSearch] = useState(false);
-  const toggleSearch = (prev: boolean) => setShowSearch(!prev);
+	const [showSearch, setShowSearch] = useState(false);
+	const toggleSearch = (prev: boolean) => setShowSearch(!prev);
 
-  const handleChange = (event: any) => {
-    router.push({ pathname: "/events", query: { search: event.target.value } });
-  };
+	const handleChange = (event: any) => {
+		router.push({
+			pathname: "/events",
+			query: { search: event.target.value },
+		});
+	};
 
-  return (
-    <div>
-      <div className="w-full flex px-[20px] md:px-[40px] justify-between items-center py-[20px] gap-3 relative">
-        <div className="flex items-center gap-[40px]">
-          <Image
-            src={Logo}
-            alt="logo"
-            width={133}
-            height={16}
-            className="h-[16px] w-[133px] object-cover"
-          />
-          <div className="w-full md:flex hidden items-center gap-4 border border-black/20 p-[10px] max-w-[350px] rounded-lg">
-            <Image src={search} alt="search" className="h-4 w-4" />
-            <input
-              onChange={handleChange}
-              type="text"
-              placeholder="Search Input"
-              className="w-full border-none outline-none"
-            />
-          </div>
-        </div>
-        <div className="flex items-center gap-[20px]">
-          <button
-            onClick={() => toggleSearch(showSearch)}
-            className="md:hidden flex"
-          >
-            <Image src={search} alt="search" className="h-5 w-5 " />
-          </button>
-          <Image src={user} alt="user" className="h-[40px] w-[40px]" />
-          <p className="font-semibold truncate">User's Name</p>
-        </div>
-      </div>
-      {showSearch && (
-        <div className="md:hidden mx-[20px] mb-2.5 items-center gap-4 border border-black/20 p-[10px] rounded-lg">
-          <input
-            value={searchQuery}
-            onChange={handleChange}
-            type="text"
-            placeholder="Search Input"
-            className="w-full border-none outline-none"
-          />
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			<div className="w-full flex px-[20px] md:px-[40px] justify-between items-center py-[20px] gap-3 relative">
+				<div className="flex items-center gap-[40px]">
+					<Image
+						src={Logo}
+						alt="logo"
+						width={133}
+						height={16}
+						className="h-[16px] w-[133px] object-cover"
+					/>
+					<div className="w-full md:flex hidden items-center gap-4 border border-black/20 p-[10px] max-w-[350px] rounded-lg">
+						<Image src={search} alt="search" className="h-4 w-4" />
+						<input
+							onChange={handleChange}
+							type="text"
+							placeholder="Search Input"
+							className="w-full border-none outline-none"
+						/>
+					</div>
+				</div>
+				<div className="flex items-center gap-[20px]">
+					<button
+						onClick={() => toggleSearch(showSearch)}
+						className="md:hidden flex"
+					>
+						<Image src={search} alt="search" className="h-5 w-5 " />
+					</button>
+					<Image
+						src={user}
+						alt="user"
+						className="h-[40px] w-[40px]"
+					/>
+					<p className="font-semibold truncate">User&apos;s Name</p>
+				</div>
+			</div>
+			{showSearch && (
+				<div className="md:hidden mx-[20px] mb-2.5 items-center gap-4 border border-black/20 p-[10px] rounded-lg">
+					<input
+						value={searchQuery}
+						onChange={handleChange}
+						type="text"
+						placeholder="Search Input"
+						className="w-full border-none outline-none"
+					/>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default EventHeader;
---
+++ components/searchFilterModal.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import ArrowDownIcon from "./icons/EventDetails/ArrowDown";
+import RedMultiply from "./icons/RedMultiply";
+import filterIcon from "@/public/assets/images/filter.svg";
+import Image from "next/image";
+import Link from "next/link";
+
+const SearchFilterModal = () => {
+	return (
+		<div className="fixed z-50 inset-0 flex justify-center items-center bg-grey-500/40 p-4">
+			<form className="bg-white p-4 rounded-2xl w-full max-w-3xl font-sans">
+				<h3 className="text-2xl font-bold font-montserrat">
+					Search Filter
+				</h3>
+				<div className="grid md:grid-cols-2 mt-6 mb-8 gap-4">
+					<div className="flex flex-col space-y-1">
+						<label
+							htmlFor="location"
+							className="font-bold text-grey-500"
+						>
+							Location
+						</label>
+						<div className="flex justify-between border border-grey-90 rounded-lg  py-2 px-3 md:py-3 md:px-5">
+							<span className="flex-1 text-center font-medium text-grey-200">
+								Location Filter
+							</span>
+							<ArrowDownIcon />
+						</div>
+					</div>
+					<div className="flex flex-col space-y-1">
+						<label
+							htmlFor="location"
+							className="font-bold text-grey-500"
+						>
+							Category
+						</label>
+						<div className="flex justify-between border border-grey-90 rounded-lg  py-2 px-3 md:py-3 md:px-5">
+							<span className="flex-1 text-center font-medium text-grey-200">
+								Category Filter
+							</span>
+							<ArrowDownIcon />
+						</div>
+					</div>
+					<div className="flex flex-col space-y-1">
+						<label
+							htmlFor="location"
+							className="font-bold text-grey-500"
+						>
+							Date
+						</label>
+						<div className="flex justify-between border border-grey-90 rounded-lg  py-2 px-3 md:py-3 md:px-5">
+							<span className="flex-1 text-center font-medium text-grey-200">
+								Date Filter
+							</span>
+							<ArrowDownIcon />
+						</div>
+					</div>
+					<div className="flex flex-col space-y-1">
+						<label
+							htmlFor="location"
+							className="font-bold text-grey-500"
+						>
+							Event Type
+						</label>
+						<div className="flex justify-between border border-grey-90 rounded-lg  py-2 px-3 md:py-3 md:px-5">
+							<span className="flex-1 text-center font-medium text-grey-200">
+								Event Type Filter
+							</span>
+							<ArrowDownIcon />
+						</div>
+					</div>
+				</div>
+				<div className="flex justify-center gap-6">
+					<button className="flex items-center px-6 py-3 md:py-4 gap-2 text-lg font-medium rounded-lg border border-secondary-300 text-secondary-500 transition-all duration-200 hover:bg-secondary-75/50 ">
+						<RedMultiply />
+						<span>Cancel</span>
+					</button>
+					<Link
+						href="/"
+						className="flex items-center px-6 py-3 md:py-4 gap-2 text-lg font-medium rounded-lg border bg-secondary-300 text-white transition-all duration-200 hover:bg-secondary-100"
+					>
+						<Image src={filterIcon} alt="filter" />
+						<span>Apply Filter</span>
+					</Link>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default SearchFilterModal;

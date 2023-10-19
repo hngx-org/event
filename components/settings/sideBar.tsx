ea31e71209ea4a90a53742215421a386addf2832
codeReaper <boluwatifetella@gmail.com>
2023-10-19 10:49:17 +0100

    Account settings layout completed

---
+++ components/settings/sideBar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import NavLink from "./navLinks";
+import {SearchIcon} from "@/public/assets/icons/settingsIcon";
+
+export default function SideBar() {
+  return (
+    <aside className="pl-20 pr-14 py-10 w-[408px] min-w-[408px] space-y-9">
+      <h3 className="text-xl text-grey-500 font-bold">Account Settings</h3>
+      {/* Search */}
+      <div className="relative">
+        <input
+          className="focus:outline-none border rounded-lg border-grey-90 py-2 px-4 w-full pl-10 placeholder:text-grey-400"
+          type="search"
+          placeholder="Search"
+          name=""
+          id=""
+        />
+        <div className="absolute left-4 top-3">
+          <SearchIcon />
+        </div>
+      </div>
+      <NavLink />
+    </aside>
+  );
+}
---
+++ pages/settings/privacy.tsx
@@ -0,0 +1,10 @@
+import AccountLayout from "@/components/layout/accountLayout";
+import React from "react";
+
+export default function Privacy() {
+  return (
+    <AccountLayout title="Privacy settings">
+      <div>Privacy</div>
+    </AccountLayout>
+  );
+}

43e4dbd112a9e97bb0e361d825d4a77ad84b232a
ebukathedev <ebuskelele@gmail.com>
2023-10-20 12:26:10 +0100

    fix styling

--- components/eventsPageTitle.tsx
+++ components/eventsPageTitle.tsx
@@ -12,7 +12,7 @@ const EventsPageTitle = ({
 	const router = useRouter();
 	return (
 		<div className="px-[20px] md:px-[40px] grid gap-4">
-			<div className="w-full mt-4 flex md:justify-between md:items-center flex-col md:flex-row gap-[15px]">
+			<div className="w-full mt-4 flex min-[480px]:justify-between min-[480px]:items-center flex-col min-[480px]:flex-row gap-[15px]">
 				<div className="gap-[16px] flex items-center">
 					<button onClick={() => router.push(".")}>
 						<Image
@@ -25,7 +25,7 @@ const EventsPageTitle = ({
 				</div>
 				<button
 					onClick={() => setIsFilterOpen(!isFilterOpen)}
-					className="w-full flex justify-center max-w-sm min-[480px]max-w-none mx-auto min-[480px]mx-0 min-[480px]justify-start items-center px-5 py-3 gap-2 text-lg font-medium rounded-lg border bg-secondary-300 text-white transition-all duration-200 hover:bg-secondary-100"
+					className="w-full flex justify-center max-w-sm min-[480px]:max-w-none mx-auto min-[480px]:mx-0 min-[480px]:justify-start min-[480px]:w-auto items-center px-5 py-3 gap-2 text-lg font-medium rounded-lg border bg-secondary-300 text-white transition-all duration-200 hover:bg-secondary-100"
 				>
 					<Image src={filterIcon} alt="filter" />
 					<p>Filter</p>

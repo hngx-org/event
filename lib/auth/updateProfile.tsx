eeadbfbcacbaf42b7bba39d95ab47cb72dca1b2c
ebukathedev <ebuskelele@gmail.com>
2023-10-20 19:28:06 +0100

    remove useless code

--- lib/auth/updateProfile.tsx
+++ lib/auth/updateProfile.tsx
@@ -1,4 +1,3 @@
-import { UserProfileProps } from "@/@types";
 import Cookies from "js-cookie";
 
 const url = process.env.NEXT_PUBLIC_API_URL;
@@ -7,23 +6,23 @@ const signUpUrl = `${url}/user/update-profile`;
 const token = Cookies.get("token");
 
 const updateProfile = async (values: any) => {
-  try {
-    const response = await fetch(signUpUrl, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: `Bearer ${token}`,
-      },
-      body: values,
-    });
-    if (!response.ok) {
-      const errorResponse = await response.json();
-      throw new Error(errorResponse.error);
-    }
-    return response.json();
-  } catch (error: any) {
-    throw new Error(error.message);
-  }
+	try {
+		const response = await fetch(signUpUrl, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "multipart/form-data",
+				Authorization: `Bearer ${token}`,
+			},
+			body: values,
+		});
+		if (!response.ok) {
+			const errorResponse = await response.json();
+			throw new Error(errorResponse.error);
+		}
+		return response.json();
+	} catch (error: any) {
+		throw new Error(error.message);
+	}
 };
 
 export default { updateProfile };

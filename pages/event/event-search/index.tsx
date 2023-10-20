57552b0db5f0739b0f17b7687ac2ca732d58c532
david2tosin <david2tosin@gmail.com>
2023-10-20 17:33:43 +0100

    Search input  component and Search Api consumption

--- pages/event/event-search/index.tsx
+++ pages/event/event-search/index.tsx
@@ -1,15 +1,39 @@
-import EventCard from "@/components/eventCard";
+import EventCard from "@/components/eventCardTimeline";
 import Link from "next/link";
 import Image from "next/image";
 import Footer from "@/components/web/footer";
 import EventHeader from "@/components/eventHeader";
+import { useRouter, useSearchParams } from "next/navigation";
+import axios, { AxiosError } from "axios";
+import { toast } from "react-toastify";
+import { useEffect, useState } from "react";
 
 export default function SearchResults() {
+  const search = useSearchParams();
+  const searchQuery = search ? search.get("q") : null;
+  const router = useRouter();
+  const goBack = (e) => {
+    e.preventDefault();
+    router.back();
+  };
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    try {
+      axios
+        .get(
+          `https://wetindeysup-api.onrender.com/api/events/search?keyword=${searchQuery}`
+        )
+        .then((res) => setEvents(res.data.data));
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  }, []);
+
   return (
     <div className="max-w-7xl w-full h-full mx-auto my-10 ">
       <EventHeader />
       <div className="w-full mt-6 flex justify-between">
-        <Link href={"#"} className="flex gap-2 items-center">
+        <Link href={"#"} onClick={goBack} className="flex gap-2 items-center">
           <Image
             src="/images/backarrow.svg"
             alt="back arrow icon"
@@ -17,7 +41,7 @@ export default function SearchResults() {
             height={24}
           />
           <p className="text-2xl font-medium">
-            Search results for <span className="font-bold">“search input”</span>
+            Search results for <span className="font-bold">{searchQuery}</span>
           </p>
         </Link>
         <Link
@@ -47,16 +71,17 @@ export default function SearchResults() {
           <p className="font-bold text-white">Filter</p>
         </Link>
       </div>
-      <div className="mt-9 mb-14 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
+      <div className="mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {events.map((event: any) => (
+          <EventCard
+            key={event.id}
+            title={event.name || "Event Name"}
+            location={event.location || "Not Specified"}
+            img={event.image || Event}
+            cost={event.ticketPrice || 0}
+            dateString={event.startTime}
+          />
+        ))}
       </div>
       <Footer />
     </div>
